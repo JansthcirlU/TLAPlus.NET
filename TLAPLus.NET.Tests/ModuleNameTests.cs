f627d4764295ccef45189372a7d2a30c42215446
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-09-15 15:46:34 +0200

    Reworked tests to test constructor and implicit string cast together. Tweaked prefix theory data for better exception message accuracy.

--- TLAPLus.NET.Tests/ModuleNameTests.cs
+++ TLAPLus.NET.Tests/ModuleNameTests.cs
@@ -8,184 +8,137 @@ public class ModuleNameTests
     [Fact]
     public void ModuleName_WhenNull_ShouldFail()
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(null!)
-        );
+        // Arrange
+        Action<string?> constructor = name => new ModuleName(name!);
+        Action<string?> implicitStringCast = name =>
+        {
+            ModuleName moduleName = name!;
+        };
+        string messageStart = "Module name must not be null.";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(() => constructor(null!));
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(() => implicitStringCast(null!));
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not be null.", ex.Message);
-    }
-
-    [Fact]
-    public void ModuleName_ImplicitStringCast_WhenNull_ShouldFail()
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(null!);
-            }
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not be null.", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 
     [Theory]
     [ClassData(typeof(WhitespaceStringData))]
-    public void ModuleName_WhenNameIsNullOrWhitespace_ShouldFail(string name)
+    public void ModuleName_WhenNameContainsWhitespace_ShouldFail(string name)
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(name)
-        );
+        // Arrange
+        Action constructor = () => new ModuleName(name);
+        Action implicitStringCast = () =>
+        {
+            ModuleName moduleName = name;
+        };
+        string messageStart = "Module name must not contain any whitespace characters.";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(constructor);
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(implicitStringCast);
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any whitespace characters.", ex.Message);
-    }
-
-    [Theory]
-    [ClassData(typeof(WhitespaceStringData))]
-    public void ModuleName_ImplicitStringCast_WhenNameIsNullOrWhitespace_ShouldFail(string name)
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(name);
-            }
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any whitespace characters.", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 
     [Theory]
     [ClassData(typeof(InvisibleCharacterData))]
     public void ModuleName_WhenNameContainsInvisibleCharacter_ShouldFail(string name)
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(name)
-        );
+        // Arrange
+        Action constructor = () => new ModuleName(name);
+        Action implicitStringCast = () =>
+        {
+            ModuleName moduleName = name;
+        };
+        string messageStart = "Module name must not contain any invisible characters.";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(constructor);
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(implicitStringCast);
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any invisible characters.", ex.Message);
-    }
-
-    [Theory]
-    [ClassData(typeof(InvisibleCharacterData))]
-    public void ModuleName_ImplicitStringCast_WhenNameContainsInvisibleCharacter_ShouldFail(string name)
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(name)
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any invisible characters.", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 
     [Theory]
     [ClassData(typeof(NewlineStringData))]
     public void ModuleName_WhenNameContainsNewline_ShouldFail(string name)
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(name);
-            }
-        );
+        // Arrange
+        Action constructor = () => new ModuleName(name);
+        Action implicitStringCast = () =>
+        {
+            ModuleName moduleName = name;
+        };
+        string messageStart = "Module name must not contain any newlines.";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(constructor);
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(implicitStringCast);
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any newlines.", ex.Message);
-    }
-
-    [Theory]
-    [ClassData(typeof(NewlineStringData))]
-    public void ModuleName_ImplicitStringCast_WhenNameContainsNewline_ShouldFail(string name)
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(name);
-            }
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not contain any newlines.", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 
     [Theory]
     [ClassData(typeof(ReservedWordsData))]
     public void ModuleName_WhenNameIsReservedWord_ShouldFail(string name)
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(name)
-        );
+        // Arrange
+        Action constructor = () => new ModuleName(name);
+        Action implicitStringCast = () =>
+        {
+            ModuleName moduleName = name;
+        };
+        string messageStart = $"Module name must not be a reserved word: {name}";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(constructor);
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(implicitStringCast);
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not be a reserved word:", ex.Message);
-    }
-
-    [Theory]
-    [ClassData(typeof(ReservedWordsData))]
-    public void ModuleName_ImplicitStringCast_WhenNameIsReservedWord_ShouldFail(string name)
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(name);
-            }
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not be a reserved word: ", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 
     [Theory]
     [ClassData(typeof(ReservedPrefixesData))]
-    public void ModuleName_WhenNameStartsWithReservedPrefix_ShouldFail(string name)
+    public void ModuleName_WhenNameStartsWithReservedPrefix_ShouldFail(string prefix, string name)
     {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () => new ModuleName(name)
-        );
+        // Arrange
+        Action constructor = () => new ModuleName(name);
+        Action implicitStringCast = () =>
+        {
+            ModuleName moduleName = name;
+        };
+        string messageStart = $"Module name must not start with a reserved prefix: {prefix}";
+
+        // Act
+        ArgumentException constructorEx = Assert.Throws<ArgumentException>(constructor);
+        ArgumentException implicitStringCastEx = Assert.Throws<ArgumentException>(implicitStringCast);
 
         // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not start with a reserved prefix: ", ex.Message);
-    }
-
-    [Theory]
-    [ClassData(typeof(ReservedPrefixesData))]
-    public void ModuleName_ImplicitStringCast_WhenNameStartsWithReservedPrefix_ShouldFail(string name)
-    {
-        // Arrange & Act
-        ArgumentException ex = Assert.Throws<ArgumentException>(
-            () =>
-            {
-                string s = new ModuleName(name);
-            }
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Module name must not start with a reserved prefix: ", ex.Message);
+        Assert.NotNull(constructorEx);
+        Assert.StartsWith(messageStart, constructorEx.Message);
+        Assert.NotNull(implicitStringCastEx);
+        Assert.StartsWith(messageStart, implicitStringCastEx.Message);
     }
 }
--- TLAPLus.NET.Tests/Seeders/Module/ReservedPrefixesData.cs
+++ TLAPLus.NET.Tests/Seeders/Module/ReservedPrefixesData.cs
@@ -2,13 +2,13 @@ using TLAPlus.NET.Modules;
 
 namespace TLAPLus.NET.Tests.Seeders.Module;
 
-public class ReservedPrefixesData : TheoryData<string>
+public class ReservedPrefixesData : TheoryData<string, string>
 {
     public ReservedPrefixesData()
     {
         foreach (string reservedPrefix in ModuleName.Forbidden.ReservedPrefixes)
         {
-            Add($"{reservedPrefix}{Guid.NewGuid()}");
+            Add(reservedPrefix, $"{reservedPrefix}{Guid.NewGuid()}");
         }
     }
 }
